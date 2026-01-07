1dc89879906770d774978fdc1f68365cc7684734
Mateo Medranda <matemedranda15@gmail.com>
2026-01-06 20:04:34 -0500

    V1.2.0 CRUD User

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
+from routers.UserRouter import router as user_router
+from database.mongodb import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    connect_to_mongo()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("shutdown")
+async def shutdown_event():
+    close_mongo_connection()
 
+app.include_router(user_router)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
--- Backend/routers/UserRouter.py
+++ Backend/routers/UserRouter.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from database.mongodb import get_db
+from services.UserService import create_user, get_user_by_email, update_user, delete_user, list_users
 from models.User import User
 
-router = APIRouter(prefix="/users", tags=["Usuarios"])
+router = APIRouter(prefix="/api/users", tags=["Users"])
 
 @router.post("/")
+async def create(usuario: User, db=Depends(get_db)):
+    return await create_user(usuario, db)
+
+@router.get("/")
+async def list(db=Depends(get_db)):
+    return await list_users(db)
+
+@router.get("/{email}")
+async def get_by_id(email: str, db=Depends(get_db)):
+    return await get_user_by_email(email, db)
+
+@router.put("/{email}")
+async def update(email: str, usuario: User, db=Depends(get_db)):
+    return await update_user(email, usuario, db)
+
+@router.delete("/{email}")
+async def delete(email: str, db=Depends(get_db)):
+    return await delete_user(email, db)
