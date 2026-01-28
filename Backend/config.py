8fe09be5dacc676a5c6329d586a3e9dd2c24e7da
unknown <matemedranda15@gmail.com>
2026-01-28 08:28:33 -0500

    Update: Cambio de imagenes locales a nube con cloudinary

--- Backend/config.py
+++ Backend/config.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import cloudinary
+import cloudinary.uploader
 
 load_dotenv()
 
@@ -12,3 +14,10 @@ LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
 MODEL_PATH = os.getenv("MODEL_PATH")
 LABEL_ENCODE_PATH = os.getenv("LABEL_ENCODE_PATH")
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
